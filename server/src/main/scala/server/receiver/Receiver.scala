238338f53b7d69e15dc9bcee94b6ede70b4f5be9
David <mail@dmaicher.de>
2015-01-17 21:35:12 +0100

    small fixes + add auth package

--- server/src/main/scala/server/receiver/Receiver.scala
+++ server/src/main/scala/server/receiver/Receiver.scala
@@ -8,7 +8,7 @@ class Receiver(val socket: Socket, val handler: Handler) {
   def listen(): Unit = {
     new Thread(new Runnable {
       override def run(): Unit = {
-        val in = new BufferedSource(socket.getInputStream()).getLines()
+        val in = new BufferedSource(socket.getInputStream).getLines()
         while(in.hasNext) {
           handler.handle(in.next())
         }
--- server/src/main/scala/server/sender/Sender.scala
+++ server/src/main/scala/server/sender/Sender.scala
@@ -4,7 +4,7 @@ import java.io.PrintStream
 import java.net.Socket
 
 class Sender(socket: Socket) {
-  val out = new PrintStream(socket.getOutputStream())
+  val out = new PrintStream(socket.getOutputStream)
   def send(message: String): Unit = {
     out.println(message)
     out.flush()
