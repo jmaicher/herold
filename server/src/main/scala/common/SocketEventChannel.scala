5e2628c6346b5d3781f058cc895eee217b257ab3
David <mail@dmaicher.de>
2015-02-03 21:59:42 +0100

    server changes

--- server/src/main/scala/common/SocketEventChannel.scala
+++ server/src/main/scala/common/SocketEventChannel.scala
@@ -1,7 +1,7 @@
 package common
 
-import java.io.PrintStream
-import java.net.Socket
+import java.io.{IOException, OutputStreamWriter, PrintStream}
+import java.net.{SocketTimeoutException, Socket}
 import java.nio.charset.CodingErrorAction
 
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -9,44 +9,48 @@ import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import com.typesafe.scalalogging.LazyLogging
 import common.event.EventDispatcher
-import common.event.socket.SocketEvent
+import common.event.socket.{AckSocketEvent, SocketEvent}
 
 import scala.io.{Codec, Source}
 
 class SocketEventChannel(val socket: Socket) extends LazyLogging {
-  private val out = new PrintStream(socket.getOutputStream)
+  private val out = new OutputStreamWriter(socket.getOutputStream, "UTF-8")
   private var listenThread: Thread = null
   private var disconnectHandler: SocketDisconnectHandler = null
+  private implicit val codec = Codec("UTF-8")
+  codec.onMalformedInput(CodingErrorAction.REPLACE)
+  codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
+  //socket.setSoTimeout(5000)
 
   def listen(dispatcher: EventDispatcher[SocketEvent]): Unit = {
     if(listenThread == null) {
       listenThread = new Thread(new Runnable {
         override def run(): Unit = {
-          implicit val codec = Codec("UTF-8")
-          codec.onMalformedInput(CodingErrorAction.REPLACE)
-          codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
           val in = Source.fromInputStream(socket.getInputStream, "UTF-8").getLines()
           logger.debug("Listening for socket events")
           try{
             while(in.hasNext) {
               val raw = in.next()
-              logger.debug("Received: "+raw)
+              //empty means heartbeat from client
+              if(!raw.isEmpty) {
+                logger.debug("Received: "+raw)
 
-              var event: SocketEvent = null
-              try{
-                event = SocketEventChannel.mapper.readValue[SocketEvent](raw)
-              }
-              catch {
-                case e: IllegalArgumentException => logger.error("Error deserializing message!", e)
-              }
+                var event: SocketEvent = null
+                try{
+                  event = SocketEventChannel.mapper.readValue[SocketEvent](raw)
+                }
+                catch {
+                  case e: IllegalArgumentException => logger.error("Error deserializing message!", e)
+                }
 
-              if(event != null) {
-                dispatcher.dispatch(event)
+                if(event != null) {
+                  dispatcher.dispatch(event)
+                }
               }
             }
           }
           catch {
-            case e: Exception => logger.error("Exception while listening: "+e)
+            case e: IOException => logger.error("IOException while listening: "+e)
           }
           finally {
             try {
@@ -66,9 +70,9 @@ class SocketEventChannel(val socket: Socket) extends LazyLogging {
   }
 
   def send(event: SocketEvent): Unit = {
-    val raw = SocketEventChannel.mapper.writeValueAsString(event)
+    val raw = SocketEventChannel.mapper.writeValueAsString(event)+"\n"
     logger.debug("Sending: "+raw)
-    out.println(raw)
+    out.write(raw, 0, raw.length)
     out.flush()
   }
 
